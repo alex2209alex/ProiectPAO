54700376674d787097c14fd262396045d0e35c56
alex2209alex <92671388+alex2209alex@users.noreply.github.com>
2023-03-26 11:35:12 +0300

    Adaugare codParafa la clasa medic

--- src/ro/fmi/pao/model/Medic.java
+++ src/ro/fmi/pao/model/Medic.java
@@ -2,13 +2,16 @@ package ro.fmi.pao.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Medic extends Persoana {
     private List<SpecializareMedic> specializari;
+    private String codParafa;
 
-    public Medic(String nume, String prenume) {
+    public Medic(String nume, String prenume, String codParafa) {
         super(nume, prenume);
         specializari = new ArrayList<>();
+        this.codParafa = codParafa;
     }
 
     public void adaugaSpecializare(SpecializareMedic specializareMedic) {
@@ -25,12 +28,34 @@ public class Medic extends Persoana {
         return specializari;
     }
 
+    public String getCodParafa() {
+        return codParafa;
+    }
+
+    public void setCodParafa(String codParafa) {
+        this.codParafa = codParafa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Medic medic = (Medic) o;
+        return codParafa.equals(medic.codParafa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codParafa);
+    }
+
     @Override
     public String toString() {
         return "Medic{" +
                 "nume='" + nume + '\'' +
                 ", prenume='" + prenume + '\'' +
-                "specializari=" + specializari +
+                ", specializari=" + specializari +
+                ", codParafa=" + codParafa +
                 '}';
     }
 }
